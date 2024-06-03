7f8bb8a0487dfab250e15cb021f324cc4b5edc6c
Dan Lavine <lavineds@gmail.com>
2024-06-02 22:22:38 -0700

    Added MergeDone
    * This takes a main context and preserves the Values assigned to it,
      while merging in any optional context's Done() calls as cancelation
      methods

--- merge.go
+++ merge.go
@@ -43,3 +43,44 @@ func MergeForDone(contexts ...context.Context) context.Context {
 
 	return oneCtx
 }
+
+//	PARAMETERS:
+//	- mainCtx - the context that all other contexts will be merged into. The Values of this context are preserved
+//	- contexts - all other contexts whos closeure of the `Done()` channel will trigger a closure of the returned contex
+//
+//	RETURNS:
+//	- context - the merged context that is canceled when any of the other contexts cancel
+//	- func() - cancel function that must be called to not leak goroutines
+//
+// MergDone will merge the 'Done()' opertation of any number of contexts into the mainCtx. The
+// values of mainCtx are preserved, but no values from any of the contexts will be preserved
+//
+// Example for when to use this operation:
+//  1. An API's http.Request.Context() has a number of `context.Value(...)` set for Open Telemitry, Logging or other metadata assigned (mainCtx)
+//  2. The same API can be canceled via a server shutdown request (...context), or some other operation that has no data assigned, just a shutdown trigger
+//  3. Now rather than passing all possible context operations around into the possible shutdown cases, they can all be merged into 1 context and handled as a single unit
+func MergeDone(mainCtx context.Context, contexts ...context.Context) (context.Context, func()) {
+	mergeCtx, oneCancel := context.WithCancel(mainCtx)
+
+	cases := []reflect.SelectCase{
+		{
+			Dir:  reflect.SelectRecv,
+			Chan: reflect.ValueOf(mainCtx.Done()),
+		},
+	}
+
+	for _, ctx := range contexts {
+		cases = append(cases, reflect.SelectCase{
+			Dir:  reflect.SelectRecv,
+			Chan: reflect.ValueOf(ctx.Done()),
+		})
+	}
+
+	go func() {
+		// don't care about any of the return values here. This just needs to be triggered
+		_, _, _ = reflect.Select(cases)
+		oneCancel()
+	}()
+
+	return mergeCtx, oneCancel
+}
--- merge_test.go
+++ merge_test.go
@@ -25,3 +25,56 @@ func Test_MergeForDone(t *testing.T) {
 		g.Eventually(oneCtx.Done).Should(BeClosed())
 	})
 }
+
+func Test_MergeDone(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	t.Run("It panics if the mainCtx is nil", func(t *testing.T) {
+		g.Expect(func() { MergeDone(nil) }).To(Panic())
+	})
+
+	t.Run("It panics if any optional contexts are nil", func(t *testing.T) {
+		g.Expect(func() { MergeDone(nil, context.Background(), nil) }).To(Panic())
+	})
+
+	t.Run("It allows just the mainCtx to be provided", func(t *testing.T) {
+		ctx, cancel := MergeDone(context.Background())
+		g.Expect(ctx).ToNot(BeNil())
+		g.Expect(cancel).ToNot(BeNil())
+	})
+
+	t.Run("It closes the context when cancel is called on the mainCtx", func(t *testing.T) {
+		ctx, cancel := MergeDone(context.Background())
+		g.Expect(ctx).ToNot(BeNil())
+		g.Expect(cancel).ToNot(BeNil())
+
+		g.Consistently(ctx.Done()).ShouldNot(BeClosed())
+
+		cancel()
+		g.Eventually(ctx.Done()).Should(BeClosed())
+	})
+
+	t.Run("It closes the context when an optional context is canceld", func(t *testing.T) {
+		optionalContext, optionalCancel := context.WithCancel(context.Background())
+
+		ctx, cancel := MergeDone(context.Background(), optionalContext)
+		g.Expect(ctx).ToNot(BeNil())
+		g.Expect(cancel).ToNot(BeNil())
+
+		g.Consistently(ctx.Done()).ShouldNot(BeClosed())
+
+		optionalCancel()
+		g.Eventually(ctx.Done()).Should(BeClosed())
+	})
+
+	t.Run("It preserves the Values on the mainCtx", func(t *testing.T) {
+		valueCtx := context.WithValue(context.Background(), "test", "true") // fine for simple test
+
+		ctx, cancel := MergeDone(valueCtx)
+		defer cancel()
+
+		g.Expect(ctx).ToNot(BeNil())
+		g.Expect(cancel).ToNot(BeNil())
+		g.Expect(ctx.Value("test")).To(Equal("true"))
+	})
+}
